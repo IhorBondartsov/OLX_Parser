5248016174aca7b2c3d4d8f79a15813e7d7a8939
IhorBondartsov <bondartsov.igor@mail.ru>
2018-08-29 13:59:42 +0300

    Add go file

--- website/check_proj.sh
+++ website/check_proj.sh
@@ -10,21 +10,33 @@ viewFolder="view"
 echo "==== START TESTING ===="
 go test --tags=integration ./...
 
+write () {
+    echo $1 >> $VFName
+}
+
 recsearch(){
     for i in `find * -maxdepth 0 -type d`
         do
         if [ -d $i ] && [ $i != $vendorFolder ] && [ $i != $viewFolder ]
             then
-                echo "Going into directory $i"
-                echo "-----------------" >> $VFName
-                echo `pwd` >> $VFName
-                echo "-----------------" >> $VFName
+                write "Going into directory $i"
+                write "-----------------"
+                write $i
+                write "VET :"
                 go tool vet $i/*.go >> $VFName
+                write "LINT :"
+                golint $i >> $VFName
+                write "GOSIMPLE :"
+                gosimple $i/... >> $VFName
+                write "STATICCHECK :"
+                staticcheck $i
                 cd $i
                 recsearch
                 cd ..
         fi
     done
 }
+
+
 
 recsearch
